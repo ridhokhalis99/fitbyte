b6cb594a91b6de7333e62372fad937c58285e9b8
Ridho Khalis <ridhokholis9a@gmail.com>
2025-09-10 07:25:08 +0700

    Initial commit

---
+++ src/main/java/com/projectsprint_solo/fitbyte/FitbyteApplication.java
@@ -0,0 +1,13 @@
+package com.projectsprint_solo.fitbyte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitbyteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitbyteApplication.class, args);
+	}
+
+}
